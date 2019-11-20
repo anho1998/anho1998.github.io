9c99934b13528686530c607c9765cb2e016bccc4
An <anibig.ptnk@gmail.com>
2019-11-19 21:08:39 -0500

    add about section with animation

--- func.js
+++ func.js
@@ -6,3 +6,15 @@ function icon_change(x){
 	else
 		show.style.height = "100%";
 }
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+	var elementTarget = document.getElementById("top");
+	if (document.body.scrollTop > (elementTarget.offsetHeight/2) || document.documentElement.scrollTop > (elementTarget.offsetHeight/2)) {
+		document.getElementById("intro").className = "show";
+	}
+	else
+	{
+		document.getElementById("intro").className = "about";
+	}
+}
